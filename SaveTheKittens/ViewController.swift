459de471030f510e4801ae58f799af60805381f9
vytenis-lengvinas <Vytenis@vyteniss-mbp.lan>
2017-01-06 12:48:41 +0200

    Initial Commit

---
+++ SaveTheKittens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheKittens
+//
+//  Created by Vytenis Lengvinas on 06/01/2017.
+//  Copyright © 2017 Vytenis Lengvinas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
